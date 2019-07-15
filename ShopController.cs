5279d24955c1b98116df1f743a4b7151f04e21c3
MarZieUnknown69 <50365589+MarZieUnknown69@users.noreply.github.com>
2019-07-15 08:45:50 +0200

    Add files via upload

---
+++ ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework_7.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopItemViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework_7.ViewModels
+{
+    public class ShopItemViewModel
+    {
+        string Name;
+        string Description;
+        double Price;
+        int QuantityAvailable;
+
+        public ShopItemViewModel()
+        {
+
+        }
+        public ShopItemViewModel(string N, string D, double P, int QA)
+        {
+            Name = N;
+            Description = D;
+            Price = P;
+            QuantityAvailable = QA;
+        }
+
+        string mName
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        string mDescription
+        {
+            get { return mName; }
+            set { mDescription = value; }
+        }
+
+        double mPrice
+        {
+            get { return mPrice; }
+            set { mPrice = value; }
+        }
+
+        int mQuantityAvailable
+        {
+            get { return mQuantityAvailable; }
+            set { mQuantityAvailable = value; }
+        }
+    }
+}
